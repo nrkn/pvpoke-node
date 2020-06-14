34a98f5c0a3b7403ec851110c27564a495a8aadf
alexameen <alex.ameen.tx@gmail.com>
2020-06-14 18:25:45 -0500

    slice-upstream-gm properly synchronized

--- src/sandbox/slice-upstream-gm.ts
+++ src/sandbox/slice-upstream-gm.ts
@@ -8,44 +8,37 @@
  * ========================================================================= */
 
 import * as fs from 'fs';
-import * as http from 'http';
 import * as path from 'path';
 import { tmpdir } from 'os';
+import * as download from 'download';
 
-const pvpokeGMURL : string = 'http://raw.githubusercontent.com/' +
+const pvpokeGMURL : string = 'https://raw.githubusercontent.com/' +
                              'pvpoke/pvpoke/master/src/data/gamemaster.json';
 
-const downloadGM = ( outfile : string ) => {
-  var f = fs.createWriteStream( outfile );
-  http.get( pvpokeGMURL, ( resp ) => {
-    resp.pipe( f );
-    f.on( 'finish', () => f.close() );
-  });
-};
+/* Add more keys here. Keys must be root keys from gamemaster.json */
+const targetKeys : string[] = [ 'moves', 'pokemon' ];
 
-const sliceKeyToFile = ( gmFile : string, key : string, outfile : string ) => {
-  fs.readFile( gmFile, ( err, data ) => {
-    if ( err ) throw err;
-    let gm : Object = data.toJSON();
-    fs.writeFile( outfile, gm[key], ( err ) => {
-      if ( err ) throw err;
-      console.log( 'Key ' + key + ' saved to: ' + outfile );
-    } );
-  } );
-};
 
-fs.mkdtemp( path.join( tmpdir(), 'gm-' ), ( err, workDir ) => {
+const downloadGM = async ( outfile : string ) : Promise<void> =>
+  fs.writeFileSync( outfile, await download( pvpokeGMURL ) );
+
+const sliceKeyToFile = async ( gmJSON : any, key : string, outfile : string ) =>
+  fs.writeFileSync( outfile, JSON.stringify( gmJSON[key] ) );
+
+
+/* Download gamemaster.json to a temporary directory, and slice keys. */
+fs.mkdtemp( path.join( tmpdir(), 'gm-' ), async ( err, workDir ) => {
   if ( err ) throw err;
-  console.log( workDir );
-  const gmPath = path.join( workDir, 'gamemaster.json' );
-  downloadGM( gmPath );
-  const targetKeys : string[] = [ 'moves', 'pokemon' ];
-  for ( let k of targetKeys ) {
-    /* FIXME make path absolute */
-    sliceKeyToFile( gmPath, k, path.join( 'dist/data/' + k + '.json' ) );
-  }
-  fs.unlink( gmPath,  ( err ) => { if ( err ) throw err;} );
-  fs.rmdir( workDir, ( err ) => { if ( err ) throw err; } );
+  const gmPath : string = path.join( workDir, 'gamemaster.json' );
+  await downloadGM( gmPath );
+  var gmData : string = await fs.readFileSync( gmPath, 'utf8' );
+  var gmJSON : any = JSON.parse( gmData );
+  await Promise.all( targetKeys.map( k => {
+    var outpath = path.join( __dirname, '../data/' + k + '.json' );
+    sliceKeyToFile( gmJSON, k, outpath );
+  } ) );
+  await fs.unlinkSync( gmPath );
+  await fs.rmdirSync( workDir );
 } );
 
 
