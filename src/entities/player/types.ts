0418b9b47d17bdd35e6f15c7e9f1fde6a861dd50
alexameen <alex.ameen.tx@gmail.com>
2020-07-08 01:32:49 -0500

    Added AI types

--- src/entities/player/types.ts
+++ src/entities/player/types.ts
@@ -5,3 +5,30 @@ export type BattlePlayer = {
   shields: 0 | 1 | 2
   switchCooldown: number
 }
+
+export enum AIStrategy {
+  DEFAULT,
+	SHIELD,
+	SWITCH_BASIC,
+	SWITCH_FARM,
+	SWITCH_ADVANCED,
+	FARM_ENERGY,
+	OVERFARM,
+	BAIT_SHIELDS,
+	WAIT_CLOCK,
+	PRESERVE_SWITCH_ADVANTAGE,
+	ADVANCED_SHIELDING,
+	BAD_DECISION_PROTECTION,
+	SACRIFICIAL_SWAP
+}
+
+export type BattleAI = BattlePlayer & {
+  name: string
+  level: number
+  chargedMoveCount: number
+  energyGuessRange: number
+  moveGuessCertainty: number
+  reactionTime: number
+  ivComboRange: number
+  strategies: AIStrategy[]
+}
