71cd9e4851fb196781b9ab3082401a6431fe95f7
alexameen <alex.ameen.tx@gmail.com>
2020-07-13 00:42:24 -0500

    Changed numeric enums to stringy enums

--- src/entities/battle/types.ts
+++ src/entities/battle/types.ts
@@ -2,10 +2,10 @@ import { BattlePlayer } from '../player/types';
 import { Pokemon } from '../pokemon/types';
 
 export enum ScenarioType {
-  BOTH_BAIT,
-  NEITHER_BAIT,
-  NO_BAIT,
-  FARM
+  BOTH_BAIT    = 'both_bait',
+  NEITHER_BAIT = 'neither_bait',
+  NO_BAIT      = 'no_bait',
+  FARM         = 'farm'
 }
 
 export type Scenario = {
@@ -23,13 +23,13 @@ export type RosterPerformance = {
 }
 
 export enum DecisionOptionType {
-  BASIC,
-  BEST,
-  COUNTER,
-  UNBALANCED,
-  SAME_TEAM,
-  SAME_TEAM_DIFFERENT_LEAD,
-  COUNTER_LAST_LEAD
+  BASIC                    = 'basic',
+  BEST                     = 'best',
+  COUNTER                  = 'counter',
+  UNBALANCED               = 'unbalanced',
+  SAME_TEAM                = 'same_team',
+  SAME_TEAM_DIFFERENT_LEAD = 'same_team_different_lead',
+  COUNTER_LAST_LEAD        = 'counter_last_lead'
 }
 
 export type DecisionOption = {
